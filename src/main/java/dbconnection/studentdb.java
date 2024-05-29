e144649d85d511b5a9be33ca7d13a17d1e0547ec
jhansi <jhansibalaga4@gmail.com>
2024-05-29 19:44:59 +0530

    adding sample project

---
+++ src/main/java/dbconnection/studentdb.java
@@ -0,0 +1,26 @@
+package dbconnection;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class studentdb {
+	private static String Driver ="com.mysql.jdbc.Driver";
+	private static String username ="root";
+	private static String password ="root";
+	private static String url ="jdbc:mysql://localhost:3306/student"; 
+	private static Connection conn = null ;
+	public static Connection getConnection() {
+		try {
+			Class.forName(Driver);
+			conn = DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	
+	
+
+}
